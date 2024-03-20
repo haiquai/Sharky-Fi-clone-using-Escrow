61236ff15be84d6debc54c0b84e20a045071bf8d
haiquai <164226278+haiquai@users.noreply.github.com>
2024-03-21 01:23:16 +0700

    Update lib.rs

--- programs/nft-lend-borrow/src/lib.rs
+++ programs/nft-lend-borrow/src/lib.rs
@@ -6,7 +6,7 @@ pub use errors::ErrorCodes;
 pub use instructions::*;
 pub use states::*;
 
-declare_id!("<PLACE YOUR ADDRESS HERE>");
+declare_id!("9rP9RYh6NV71RWBoPWiAiacJtN39ia53HwbNoEjVnYsv");
 
 #[program]
 pub mod nft_lend_borrow {
